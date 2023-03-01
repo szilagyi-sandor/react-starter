9eebc5c25791e2f40fef23b86a515eabb234545b
Sándor Szilágyi <sandormark.szilagyi@nestle.com>
2023-03-01 17:05:54 +0100

    feat: add better svg import support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="vite/client" />
 
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [svgr(), react(), tsconfigPaths()],
 });
