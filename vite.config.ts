ec8020a8d6b5c56030f4b210e061fc9c6bc28eb2
Sándor Szilágyi <sandormark.szilagyi@nestle.com>
2023-03-01 17:11:25 +0100

    feat: add error checker plugin to vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,17 @@
 import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), tsconfigPaths()],
+  plugins: [
+    svgr(),
+    react(),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+    }),
+  ],
 });
