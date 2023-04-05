5486c857eb42dc7a8b11811d5775eb4e3b1c5af6
Sándor Szilágyi <sandormark.szilagyi@nestle.com>
2023-04-05 16:09:08 +0200

    feat: add appsettings to the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { ErrorPage } from '@pages';
 import ReactDOM from 'react-dom/client';
+import { getAppsettings, AppsettingsProvider } from '@features/appsettings';
 import { App } from './App';
 import { ErrorBoundary } from './shared/error';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ErrorBoundary fallback={<ErrorPage />}>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
-);
+const startApp = async () => {
+  const appsettings = await getAppsettings();
+
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <ErrorBoundary fallback={<ErrorPage />}>
+        <AppsettingsProvider value={appsettings}>
+          <App />
+        </AppsettingsProvider>
+      </ErrorBoundary>
+    </React.StrictMode>
+  );
+};
+
+startApp();
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,13 @@
+import { useAppsettingsContext } from '@features/appsettings';
+
 function HomePage() {
+  const { customMessage } = useAppsettingsContext();
   return (
     <section>
       <header>
         <h2>Home page</h2>
+
+        <p>{customMessage}</p>
       </header>
     </section>
   );
